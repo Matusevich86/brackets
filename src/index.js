4f9b487b522c389d8f7cfd2614b1834cb3248ecf
Matusevich86 <alex.matusevich86@gmail.com>
2023-02-11 18:10:14 +0100

    feat: start task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let arrStak = [];
+  let aStr = {};
+  bracketsConfig.forEach(elem => {
+    let key = elem[0].toString()
+    let value = elem[1]
+    aStr[key]=value
+    }) 
+
+  for(let bracket of str) {
+    if(bracket in aStr) {
+      arrStak.push(bracket)
+    } else {
+      if(arrStak.length == 0) {
+        return false
+      }
+    
+    let lastBracket = arrStak[arrStak.length -1];
+    
+    if(aStr[lastBracket] === bracket) {
+      arrStak.pop()
+      } else {
+        return false
+        }
+    }
+  }
+  return arrStak.length == 0
 }
